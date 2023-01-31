4ff28bb1cfecffc10a84fc64228c3a811808fa1e
Ethan Ho <ethan.n.ho@gmail.com>
2023-01-31 13:09:38 -0600

    Add quantile strat as pytest

--- tests/test_backtest.py
+++ tests/test_backtest.py
@@ -1,7 +1,11 @@
+import os
 import pytest
 import pandas as pd
 import plotly.express as px
-from ubacktester import PriceFeed, BacktestEngine, BasicStrategy, px_plot, BuyAndHold
+from ubacktester import (
+    PriceFeed, BacktestEngine, BasicStrategy, px_plot, BuyAndHold,
+    NaiveQuantileStrat
+)
 
 
 class TestPriceFeed:
@@ -48,6 +52,8 @@ class TestPriceFeed:
 
 
 class TestRunStrategy:
+    HW3_QUANTILES_CSV = 'tests/data/hw3_quantiles.csv'
+    HW3_PRICES_CSV = 'tests/data/hw3_prices.csv'
 
     def test_run_basic_strategy(self):
         be = BacktestEngine(
@@ -98,3 +104,33 @@ class TestRunStrategy:
             include_cols=['daily_pct_returns'],
             # scale_cols={'nshort': 40, 'nlong': 40}
         )
+
+    def _get_quantiles(self):
+        df = pd.read_csv(self.HW3_QUANTILES_CSV).set_index('date').sort_index()
+        return df
+
+    def _get_prices(self):
+        df = pd.read_csv(self.HW3_PRICES_CSV).set_index('date').sort_index()
+        return df
+
+    @pytest.mark.parametrize('ratio,start_date,end_date', [
+        ('pe', '2015-01-01', '2015-02-01'),
+    ])
+    @pytest.mark.skipif(
+        not (os.path.isfile(HW3_PRICES_CSV) and os.path.isfile(HW3_QUANTILES_CSV)),
+        reason="requires tests/data/hw3_*.csv files"
+    )
+    def test_quantile_strat(self, ratio, start_date, end_date) -> pd.DataFrame:
+        be = BacktestEngine(
+            start_date=start_date,
+            end_date=end_date,
+        )
+        quantiles_feed = PriceFeed.from_df(self._get_quantiles().loc[start_date:end_date])
+        prices_feed = PriceFeed.from_df(self._get_prices().loc[start_date:end_date].reset_index())
+        be.add_feed(quantiles_feed, name='quantiles')
+        be.add_feed(prices_feed, name='prices')
+        strat = NaiveQuantileStrat(cash_equity=1e4, ratio=ratio)
+        strat.name = ratio
+        be.add_strategy(strat)
+        be.run()
+        return strat.df.reset_index()
