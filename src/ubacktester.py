ebfe86554fc84aac5689c3cda0ea0c39611b3e90
Ethan Ho <ethan.n.ho@gmail.com>
2023-02-01 09:50:36 -0600

    Progress on HW4

--- src/ubacktester.py
+++ src/ubacktester.py
@@ -14,7 +14,6 @@ import numpy as np
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
-from profiler import profiler
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger()
